df6ecba3dd06d163fc7b38684ad8193a12770125
InsaneReaper7 <INSANE.REAPER7@GMAIL.COM>
2026-03-26 20:23:11 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Game } from './components/Game.jsx';
-
-const root = ReactDOM.createRoot(document.getElementById('game-root'));
-root.render(React.createElement(Game));
